c25b6c5376c34eef35358e99d0ac813f5fbd5af2
Daniel Strong <danielstrong@mac.com>
2025-06-27 10:58:13 -0800

    update to use system argument

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-import os
+import os, sys
+from google import genai
 from dotenv import load_dotenv
 
+if not sys.argv[1]:
+    print("Error: invalid input", file=sys.stderr)
+    sys.exit(1)
+else:
+    query = sys.argv[1]
+    
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=query
+)
+print(response.text)
+print('Prompt tokens: ' + str(response.usage_metadata.prompt_token_count))
+print('Response tokens: ' + str(response.usage_metadata.candidates_token_count))
